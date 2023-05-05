9d775ce1d1c4eaf9c77a6d76714e75dfbd3ce8c4
Summer Xiao <summerxiao0328@gmail.com>
2023-05-05 19:25:26 +0800

    .js

--- src/index.js
+++ src/index.js
@@ -209,7 +209,7 @@ function Suicide(){
                     <GeoChart data_map={data_map_abs} property={year_lst[year]} height={HEIGHT/1.05} width={WIDTH} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry}/>   
                 </div>
                 <div className='col-lg-6'>
-                <ScatterPlot2 data={data} offsetX={heatmap_margin.left} offsetY={heatmap_margin.top} height={heatmap_height} width={heatmap_width}
+                    <ScatterPlot2 data={data} offsetX={heatmap_margin.left} offsetY={heatmap_margin.top} height={heatmap_height} width={heatmap_width}
                         selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry}
                         setLeft={setLeft} setTop={setTop}
                     />
